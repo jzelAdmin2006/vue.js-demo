a0d9330ee1c269e304197d79efaefd3f493457ef
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-03-12 10:20:25 +0100

    add data group

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ new Vue({
     el: '#app',
     data: {
         name: 'jzelAdmin2006',
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+
+        person: {
+            name: 'jzelAdmin2006',
+            height: 1.85,
+        }
     },
 });
