b6128feb37ff42e61d7fcba8c83d53feec00ebb4
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-03-12 10:36:13 +0100

    add html binding example

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ new Vue({
             height: 1.85,
         },
         website: 'https://github.com/jzelAdmin2006',
+        websiteTag: '<a href="https://github.com/jzelAdmin2006">Go to best GitHub user (tag).</a>'
     },
 
     methods: {
